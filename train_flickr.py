d78623bd02c700bf1756ed3577a801c47e57d92f
Casper Guo <casperg@umich.edu>
2023-05-15 17:09:47 -0400

    Rewrite gitignore, remove all models from memory

--- train_flickr.py
+++ train_flickr.py
@@ -232,11 +232,11 @@ def generate_images(model, test_input, tar):
     prediction = model(test_input, training=True)
     plt.figure(figsize=(15, 15))
 
-    display_list = [test_input[0], tar[0], prediction[0]]
-    title = ['1960s', '2020s', 'Some time in between']
+    display_list = [test_input[0], prediction[0]]
+    title = ['Original', 'Upgraded']
 
-    for i in range(3):
-        plt.subplot(1, 3, i+1)
+    for i in range(2):
+        plt.subplot(1, 2, i+1)
         plt.title(title[i])
         # Getting the pixel values in the [0, 1] range to plot.
         plt.imshow(display_list[i] * 0.5 + 0.5)
@@ -323,13 +323,15 @@ checkpoint.restore("./flickr_checkpoints/ckpt-12")
 
 TEST_DIR_BW = pathlib.Path(__file__).parent / "test_images" / "bw"
 TEST_DIR_COLOR = pathlib.Path(__file__).parent / "test_images" / "color"
+CHERRIES = pathlib.Path(__file__).parent / "test_images" / "cherries"
 
-# for path in os.listdir():
-#     path = path.lower()
-#     if path.endswith('.jpg') or path.endswith('.jpeg'):
-#         bw, target = load_test_image(path)
 
-#         bw = tf.expand_dims(bw, axis=0)
-#         target = tf.expand_dims(target, axis=0)
+for path in os.listdir(CHERRIES):
+    path = path.lower()
+    if path.endswith('.jpg') or path.endswith('.jpeg'):
+        bw, target = load_test_image(str(CHERRIES / path))
 
-#         generate_images(generator, bw, target)
+        bw = tf.expand_dims(bw, axis=0)
+        target = tf.expand_dims(target, axis=0)
+
+        generate_images(generator, bw, target)
